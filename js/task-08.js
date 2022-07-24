df3331fc3fbea531f3280e1a0f7c81808f30a3fb
Nikolay Tankov <tankov.n.k@gmail.com>
2022-07-24 20:56:22 +0300

    task 8 - done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,26 @@
+const inputForm = document.querySelector(".login-form");
+inputForm.addEventListener("submit", onFormSubmit);
 
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const formElements = event.currentTarget.elements;
+
+  const mail = formElements.email.value;
+  const password = formElements.password.value;
+
+  const formData = { mail, password };
+
+  if (mail === "" || password === "") {
+    alert(`Пожалуйста заполните все поля MAIL и PASSWORD !`);
+  } else {
+    console.log(formData);
+    inputForm.reset();
+  }
+}
+
+// Обработка отправки формы form.login-form должна быть по событию submit.
+// При отправке формы страница не должна перезагружаться.
+// Если в форме есть незаполненные поля, выводи alert с предупреждением о том, что все поля должны быть заполнены.
+// Если пользователь заполнил все поля и отправил форму, собери значения полей в обьект, где имя поля будет именем свойства, а значение поля - значением свойства. Для доступа к элементам формы используй свойство elements.
+// Выведи обьект с введенными данными в консоль и очисти значения полей формы методом reset.
