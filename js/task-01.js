46e8619c8e3c4944b51f0858213a68411fb5a621
Nikolay Tankov <tankov.n.k@gmail.com>
2022-07-25 21:40:51 +0300

    25.07.-2

--- js/task-01.js
+++ js/task-01.js
@@ -15,3 +15,21 @@ navEl.forEach((element) => {
   console.log(`Category: ${element.firstElementChild.textContent}`);
   console.log(`Elements: ${element.lastElementChild.children.length}`);
 });
+
+// -----------------------------------------------------------------------------
+// Напиши скрипт который:
+
+// Посчитает и выведет в консоль количество категорий в ul#categories, то есть элементов li.item.
+// Для каждого элемента li.item в списке ul#categories, найдет и выведет в консоль текст заголовка элемента (тега <h2>) и количество элементов в категории (всех вложенных в него <li>).
+// В результате, в консоли будут выведены такие сообщения.
+
+// Number of categories: 3
+
+// Category: Animals
+// Elements: 4
+
+// Category: Products
+// Elements: 3
+
+// Category: Technologies
+// Elements: 5
